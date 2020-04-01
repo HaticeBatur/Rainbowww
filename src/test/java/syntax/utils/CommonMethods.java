711b44e929ab146ee1d2fb9b1f30d8d469ac8fa7
Hatice Batur <hb.baturhatice@gmail.com>
2020-04-01 17:22:45 -0500

    second

--- src/test/java/syntax/utils/CommonMethods.java
+++ src/test/java/syntax/utils/CommonMethods.java
@@ -3,5 +3,6 @@ package syntax.utils;
 public class CommonMethods {
 	public static void main(String[] args) {
 		System.out.println("NEW PROJECT");
+		System.out.println("Change inside WORK branch");
 	}
 }
