bb0afefaafd36202ef487ad991b25d91d3c2bcb9
Hatice Batur <hb.baturhatice@gmail.com>
2020-04-02 00:08:37 -0500

    stash 1

--- src/test/java/syntax/utils/CommonMethods.java
+++ src/test/java/syntax/utils/CommonMethods.java
@@ -4,5 +4,6 @@ public class CommonMethods {
 	public static void main(String[] args) {
 		System.out.println("NEW PROJECT");
 		System.out.println("Change inside WORK branch");
+		System.out.println("Stash 1");
 	}
 }
